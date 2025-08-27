bdd89961e70ac3236ced009f1f63066e7105f456
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-08-27 15:08:03 -0300

    Exercício 1 Array finalizado

--- Collection/exercicio1Array.ts
+++ Collection/exercicio1Array.ts
@@ -1,1 +1,22 @@
-import readlinesync = require("readline-sync");
+import leia = require("readline-sync");
+
+let cores: Array<string> = new Array<string>();
+let i: number;
+let cor: string;
+
+for(i = 0; i < 5; i++){
+    cor = leia.question('Digite uma cor: ');
+    cores.push(cor)
+}
+
+console.log('\nListar todas as cores:\n');
+for(cor of cores){
+    console.log(cor)
+}
+
+console.log('\nOrdenar as cores:\n');
+cores.sort()
+
+for(cor of cores){
+    console.log(cor)
+}
